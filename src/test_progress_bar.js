6defe3a3455e3ccb05bcd36256652e978e054901
StarsLikeDust <curranistan@gmail.com>
2015-08-22 14:33:10 -0400

    Added Test Counter

--- src/test_progress_bar.js
+++ src/test_progress_bar.js
@@ -1,10 +1,14 @@
+var counter = 0;
+
 function bargo(){
     var line = new ProgressBar.Line('#example-line-container', {
       color: '#FCB03C',
-      duration: 5000
+      duration: 2000
 
     });
     line.animate(1, function() {
         line.animate(0);
+        counter +=1; //increment counter
+        document.getElementById('testCounter').innerHTML = counter; //change counter on index.html
     })
   }
