265cce6824bdf20e6613a8299a9215317d8e010c
Sam Backus <Samuel.Backus@gmail.com>
2015-08-22 14:04:35 -0400

    make progress bar work

--- src/test_progress_bar.js
+++ src/test_progress_bar.js
@@ -1,17 +1,10 @@
-$.ready(function(){
+function bargo(){
+    var line = new ProgressBar.Line('#example-line-container', {
+      color: '#FCB03C',
+      duration: 5000
 
-  var line = new ProgressBar.Line('#example-line-container', {
-    color: '#FCB03C',
-    duration: 5000
-
-  });
-
-  function bargo(){
+    });
     line.animate(1, function() {
         line.animate(0);
     })
   }
-
-  $("#goButton").addEventListener("click", bargo);
-
-});
